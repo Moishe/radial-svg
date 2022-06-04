e05d2a77b5629dc109b048b77b7bbbaad9413fd8
Moishe Lettvin <moishel@gmail.com>
2022-06-04 11:44:16 -0600

    Integrate vpype call into python script, couple other tweaks

--- make-svg.py
+++ make-svg.py
@@ -1,53 +1,104 @@
 import math
+import os
 import svgwrite
 from svgwrite import cm, mm
 
 layers = [
     {
-        'steps': 128,
+        'steps': 256,
         'circles': [
             {
-                'radius': 20,
+                'radius': 90,
                 'speed': 1.0,
                 'offset': math.pi,
                 'epicycle': {
-                    'radius': 0,
-                    'speed': 0,
+                    'radius': 3,
+                    'speed': 12,
                     'offset': 0
                 }
             },
             {
-                'radius': 10,
+                'radius': 70,
                 'speed': 1.0,
-                'offset': 0,
+                'offset': math.pi,
                 'epicycle': {
-                    'radius': 0,
-                    'speed': 0,
+                    'radius': 20,
+                    'speed': 9,
                     'offset': 0
                 }
             },
         ]
     },
     {
-        'steps': 512,
+        'steps': 256,
         'circles': [
             {
-                'radius': 20,
+                'radius': 70,
                 'speed': 1.0,
                 'offset': math.pi,
                 'epicycle': {
-                    'radius': 0,
-                    'speed': 0,
+                    'radius': 20,
+                    'speed': 9,
                     'offset': 0
                 }
             },
             {
-                'radius': 35,
+                'radius': 50,
                 'speed': 1.0,
                 'offset': math.pi,
                 'epicycle': {
                     'radius': 5,
-                    'speed': 20,
+                    'speed': 4,
+                    'offset': 0
+                }
+            },
+        ]
+    },
+    {
+        'steps': 256,
+        'circles': [
+            {
+                'radius': 50,
+                'speed': 1.0,
+                'offset': math.pi,
+                'epicycle': {
+                    'radius': 5,
+                    'speed': 4,
+                    'offset': 0
+                }
+            },
+            {
+                'radius': 30,
+                'speed': 1.0,
+                'offset': math.pi,
+                'epicycle': {
+                    'radius': 3,
+                    'speed': 11,
+                    'offset': 0
+                }
+            },
+        ]
+    },
+    {
+        'steps': 128,
+        'circles': [
+            {
+                'radius': 30,
+                'speed': 1.0,
+                'offset': math.pi,
+                'epicycle': {
+                    'radius': 3,
+                    'speed': 11,
+                    'offset': 0
+                }
+            },
+            {
+                'radius': 5,
+                'speed': -2.0,
+                'offset': math.pi,
+                'epicycle': {
+                    'radius': 0,
+                    'speed': 0,
                     'offset': 0
                 }
             },
@@ -63,11 +114,12 @@ def get_xy(c, ratio, oxy):
         (x, y) = get_xy(c['epicycle'], ratio, (x, y))
     return (x, y)
 
-def make_svg(filename):
+def make_svg(dir, filename):
     prev_x = None
     prev_y = None
     for (idx, layer) in enumerate(layers):
-        layer_filename = "%s-%d.svg" % (filename, idx)
+        print("Processing circle %d" % idx)
+        layer_filename = "%s/%s-%d.svg" % (dir, filename, idx)
         dwg = svgwrite.Drawing(filename=layer_filename, debug=True, height='210mm', width='297mm')
         lines = dwg.add(dwg.g(id='lines', stroke='black', stroke_width='0.1'))
 
@@ -84,6 +136,9 @@ def make_svg(filename):
                 prev_y = y
 
         dwg.save()
+        print("vpype processing")
+        new_filename = "%s/proc-%s-%d.svg" % (dir, filename, idx)
+        os.system("vpype read %s linemerge reloop linesort write %s" % (layer_filename, new_filename))
 
 if __name__ == "__main__":
-    make_svg('output/test-flower')
+    make_svg('output', 'test-flower')
--- vp.sh
+++ vp.sh
@@ -1,1 +1,1 @@
-vpype read output.svg linemerge reloop linesort write output-new.svg
+vpype read output/*.svg linemerge reloop linesort write output/new-*.svg
